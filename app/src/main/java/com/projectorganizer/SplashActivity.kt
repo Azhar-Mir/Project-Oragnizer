0adc6d20913ba07581eb92506d60ce736112b10e
Azhar-Mir <77479975+Azhar-Mir@users.noreply.github.com>
2021-07-24 15:14:51 +0530

    Splash Screen, SignUp & SignIn Screen

---
+++ app/src/main/java/com/projectorganizer/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.projectorganizer
+
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.projectorganizer.R
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val typeface: Typeface =
+            Typeface.createFromAsset(assets, "allura_regular.ttf")
+        tv_app_name.typeface = typeface
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+            finish()
+        }, 2500)
+    }
+}
